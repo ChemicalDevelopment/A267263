dccf37fd8688e1ccb082f03c733f46cc490bb759
CadeBrown <brown.cade@gmail.com>
2016-08-15 09:21:52 -0400

    Updated readme + comments

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ make
 ./A267263.o
 ```
 And you are running it.
+
+## Explanation
+Primorial base is similar to most bases. We use base 10, computers use base 2.
+Every place value is the last multiplied by 10
+100 = 10 * 10 (you add 0)
+(100)_(2) = (10)_(2) * (10)_(2) In base 2
+But, in primorial base, the place value is multiplied by the next prime.
+So, it looks like this:
+...(2*3*5's place)(2*3's place)(2's place)(1's place)
+This sequence is the number of places where the value is not 0.
+
+For example, (5)_(10) = (21)_(#)
+(6)_(10) = (100)_(#)
+And A267363_6 = 1, because (100)_(#) has 1 nonzero digit
--- core.c
+++ core.c
@@ -2,8 +2,10 @@
 #include <math.h>
 #include "core.h"
 
+//We store primorials here, to iterate over.
 long long int prim[13] = { 1, 2, 6, 30, 210, 2310, 30030, 510510, 9699690, 223092870, 6469693230, 200560490130, 7420738134810  };
 
+//Computes a single value, at n, and stores it in byte size
 int8_t compute_single(long long int n) {
     long long int residue = n;
     int8_t ret = 0;
@@ -17,7 +19,7 @@ int8_t compute_single(long long int n) {
     return ret;
 }
 
-
+//Computes 0 - N (inclusive) and stores it in a int8 array (byte array)
 void compute(long long int N, int8_t *out) {
     long long int i;
     for (i = 0; i <= N; ++i) {
